b66f6afefc74012211fa39e25d58b60a71dbe558
Lucas Ricci <lucasrafael.ricci@gmail.com>
2024-06-07 15:59:01 -0300

    feat: setup do projeto com shadcn/ui, eslint, prettier, react router dom e react helmet

---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      return <h1>Dashboard</h1>
+    </>
+  )
+}
