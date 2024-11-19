b1c95606311bda0648e6be390cbd5ee5c5f90585
Lucas Rafael Ricci <lucasrafael.ricci@gmail.com>
2024-11-19 07:01:35 -0300

    feat: add componente cabeçalho

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+import { Pizza } from 'lucide-react'
+
+import { NavLink } from './nav-link'
+import { Separator } from './ui/separator'
+
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-4">
+        <Pizza className="h-6 w-6" />
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">Início</NavLink>
+          <NavLink to="/orders">Pedidos</NavLink>
+        </nav>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/app/orders.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <h1>Pedidos</h1>
+    </>
+  )
+}
