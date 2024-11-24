b1f11d2e609b92f5a40973e677ecdef1ec32534a
Lucas Rafael Ricci <lucasrafael.ricci@gmail.com>
2024-11-24 12:42:46 -0300

    build: add configs para comunicação entre front e back.

--- pizzashop-front/src/App.tsx
+++ pizzashop-front/src/App.tsx
@@ -1,10 +1,12 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 export function App() {
   return (
@@ -12,7 +14,9 @@ export function App() {
       <Helmet titleTemplate="%s | pizza.shop" />
       <Toaster richColors />
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
-        <RouterProvider router={router}></RouterProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}></RouterProvider>
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
